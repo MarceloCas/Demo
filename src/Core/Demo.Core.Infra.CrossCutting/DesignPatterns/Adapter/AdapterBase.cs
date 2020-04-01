55e7ec030dfc8875e166a26329b7532b4bb8f985
Marcelo Castelo <marcelo.castelo@lincros.com>
2020-04-01 07:54:02 -0300

    include basic adapter structure

---
+++ src/Core/Demo.Core.Infra.CrossCutting/DesignPatterns/Adapter/AdapterBase.cs
@@ -0,0 +1,18 @@
+﻿using Demo.Core.Infra.CrossCutting.DesignPatterns.Adapter.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Core.Infra.CrossCutting.DesignPatterns.Adapter
+{
+    public abstract class AdapterBase<TTo, TFrom>
+        : IAdapter<TTo, TFrom>
+    {
+        public abstract TTo Adaptee(TFrom source);
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ src/Core/Demo.Core.Infra.CrossCutting/DesignPatterns/Adapter/Interfaces/IAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Core.Infra.CrossCutting.DesignPatterns.Adapter.Interfaces
+{
+    public interface IAdapter<TTo, TFrom>
+        : IDisposable
+    {
+        TTo Adaptee(TFrom source);
+    }
+}
