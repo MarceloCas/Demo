cae8cb8db8be07a747380b5d1b22e43bd22eba85
Marcelo Castelo <marcelo.castelo@lincros.com>
2020-04-10 04:35:46 -0300

    project fixes

---
+++ src/InvoiceImporter/Demo.InvoiceImporter.Presentation.WebApp/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Syncfusion.Blazor;
+
+namespace Demo.InvoiceImporter.Presentation.WebApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Add your license key here");
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddBaseAddressHttpClient();
+            await builder.Build().RunAsync();
+        }
+    }
+}
