92e5c3d2dd2560129fc404f2c508d0fd1bcc65ec
Marcelo Castelo <marcelo.castelo@lincros.com>
2020-04-12 05:56:13 -0300

    Include others domain models validations

--- src/InvoiceImporter/Demo.InvoiceImporter.Domain/DomainServices/InvoiceDomainService.cs
+++ src/InvoiceImporter/Demo.InvoiceImporter.Domain/DomainServices/InvoiceDomainService.cs
@@ -1,6 +1,7 @@
 ﻿using Demo.Core.Infra.CrossCutting.DesignPatterns.Bus.Interfaces;
 using Demo.InvoiceImporter.Domain.DomainModels;
 using Demo.InvoiceImporter.Domain.DomainModels.Factories.Interfaces;
+using Demo.InvoiceImporter.Domain.DomainModels.Validations.Invoices.Interfaces;
 using Demo.InvoiceImporter.Domain.DomainServices.Base;
 using Demo.InvoiceImporter.Domain.DomainServices.Interfaces;
 using Demo.InvoiceImporter.Domain.Events.Invoices.Factories.Interfaces;
@@ -12,23 +13,29 @@ namespace Demo.InvoiceImporter.Domain.DomainServices
         : DomainServiceBase<Invoice>,
         IInvoiceDomainService
     {
+        private readonly IInvoiceIsValidForImportValidation _invoiceIsValidForImportValidation;
+
         private readonly IInvoiceWasImportedEventFactory _invoiceWasImportedEventFactory;
 
         public InvoiceDomainService(
             IBus bus,
             IInvoiceFactory invoiceFactory,
+            IInvoiceIsValidForImportValidation invoiceIsValidForImportValidation,
             IInvoiceWasImportedEventFactory invoiceWasImportedEventFactory
             ) 
             : base(bus, invoiceFactory)
         {
+            _invoiceIsValidForImportValidation = invoiceIsValidForImportValidation;
             _invoiceWasImportedEventFactory = invoiceWasImportedEventFactory;
         }
 
         public async Task<Invoice> ImportInvoiceAsync(string tenantCode, string creationUser, Invoice invoiceToImport)
         {
-            // Validation
+            /* Validate */
+            if (await ValidateAsync(invoiceToImport, _invoiceIsValidForImportValidation) == false)
+                return invoiceToImport;
 
-            // Process
+            /* Process */
             var importedInvoice = (await Factory.CreateAsync()).ImportInvoice(
                 tenantCode,
                 invoiceToImport.Code,
@@ -37,7 +44,7 @@ namespace Demo.InvoiceImporter.Domain.DomainServices
                 invoiceToImport.InvoiceItemCollection,
                 creationUser);
 
-            // Notify
+            /* Notify */
             _ = await Bus.SendEventAsync(await _invoiceWasImportedEventFactory.CreateAsync(importedInvoice));
 
             return importedInvoice;
--- src/InvoiceImporter/Demo.InvoiceImporter.Domain/DomainServices/ProductDomainService.cs
+++ src/InvoiceImporter/Demo.InvoiceImporter.Domain/DomainServices/ProductDomainService.cs
@@ -1,6 +1,7 @@
 ﻿using Demo.Core.Infra.CrossCutting.DesignPatterns.Bus.Interfaces;
 using Demo.InvoiceImporter.Domain.DomainModels;
 using Demo.InvoiceImporter.Domain.DomainModels.Factories.Interfaces;
+using Demo.InvoiceImporter.Domain.DomainModels.Validations.Products.Interfaces;
 using Demo.InvoiceImporter.Domain.DomainServices.Base;
 using Demo.InvoiceImporter.Domain.DomainServices.Interfaces;
 using Demo.InvoiceImporter.Domain.Events.Products.Factories.Interfaces;
@@ -12,33 +13,39 @@ namespace Demo.InvoiceImporter.Domain.DomainServices
         : DomainServiceBase<Product>,
         IProductDomainService
     {
+        private readonly IProductIsValidForImportValidation _productIsValidForImportValidation;
+
         private readonly IProductWasImportedEventFactory _productWasImportedEventFactory;
         private readonly IProductWasUpdatedEventFactory _productWasUpdatedEventFactory;
 
         public ProductDomainService(
             IBus bus,
             IProductFactory productFactory,
+            IProductIsValidForImportValidation productIsValidForImportValidation,
             IProductWasImportedEventFactory productWasImportedEventFactory,
             IProductWasUpdatedEventFactory productWasUpdatedEventFactory
             ) 
             : base(bus, productFactory)
         {
+            _productIsValidForImportValidation = productIsValidForImportValidation;
             _productWasImportedEventFactory = productWasImportedEventFactory;
             _productWasUpdatedEventFactory = productWasUpdatedEventFactory;
         }
 
         public async Task<Product> ImportProductAsync(string tenantCode, string creationUser, Product productToImport)
         {
-            // Validation
+            /* Validate */
+            if (await ValidateAsync(productToImport, _productIsValidForImportValidation) == false)
+                return productToImport;
 
-            // Process
+            /* Process */
             var importedProduct = (await Factory.CreateAsync()).ImportProduct(
                 tenantCode,
                 productToImport.Name,
                 productToImport.Code,
                 creationUser);
 
-            // Notify
+            /* Notify */
             _ = await Bus.SendEventAsync(await _productWasImportedEventFactory.CreateAsync(importedProduct));
 
             return importedProduct;
